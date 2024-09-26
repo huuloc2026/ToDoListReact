cc28a2bfc7fd557190c4261af93813f887b6a0b6
huuloc2026 <huuloc2026@gmail.com>
2024-09-26 17:21:16 +0700

    edit file todolist edit key

--- src/components/ToDoData.jsx
+++ src/components/ToDoData.jsx
@@ -1,14 +1,13 @@
 const ToDoData = (props) => {
   /// props la object
-  // console.log(listJobData);
-  // const { toDoList } = props;
-  console.log(props.toDoList);
+  const { toDoList } = props;
+
   return (
     <ul className="">
-      {props.toDoList.map((item) => (
-        <li key={item.id}>{item.job}</li>
-      ))}
-      <div>{JSON.stringify(props.toDoList)}</div>
+      {toDoList.map((item) => {
+        return <li key={item.id}>{item.job}</li>;
+      })}
+      <div>{JSON.stringify(toDoList)}</div>
     </ul>
   );
 };
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -11,7 +11,7 @@ const TodoList = () => {
   const addNewToDo = (nameTodo) => {
     const newtoDo = {
       id: random(1, 1000000),
-      name: nameTodo,
+      job: nameTodo,
     };
     setLisTodoList((prev) => [...prev, newtoDo]);
   };
